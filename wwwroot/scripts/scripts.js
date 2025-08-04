3fe637279bec08279bbf0a1491fd28d5459113bb
JRieder <Jordan_Rieder@baylor.edu>
2025-08-04 07:22:18 -0500

    some changes

--- wwwroot/scripts/scripts.js
+++ wwwroot/scripts/scripts.js
@@ -1,3 +1,16 @@
-﻿window.scrollToBottom = (element) => {
-    element.scrollTop = element.scrollHeight;
+﻿window.scrollToBottom = () => {
+    const scrollTarget = document.documentElement || document.body;
+
+    const tryScroll = (attemptsLeft) => {
+        if (scrollTarget.scrollHeight > 0) {
+            scrollTarget.scrollTo({
+                top: scrollTarget.scrollHeight,
+                behavior: 'smooth'
+            });
+        } else if (attemptsLeft > 0) {
+            setTimeout(() => tryScroll(attemptsLeft - 1), 50);
+        }
+    };
+
+    tryScroll(5); // Retry up to 5 times
 };
